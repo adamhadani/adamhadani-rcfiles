091ea3f1587da1796ecb681e9ab90974745937ef
Adam Ever-Hadani <adamhadani@gmail.com>
2010-09-03 13:51:22 -0700

    added setup-dev-machine script

---
+++ setup-dev-machine.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# adamhadani's Ubuntu dev machine setup bootstrap script
+#
+# Tested on an Ubuntu 9.10 & 10.04 Server LTS installations.
+#
+# Author: Adam Ever-Hadani <search@videosurf.com>
+# URL:    http://github.com/adamhadani/adamhadani-rcfiles
+#
+# Last Changes:
+#  09/03/10 - Initial version commited.
+#
+#
+#
+###############################################################################
+
+log() {
+    echo "$*"
+}
+error() {
+    printf "ERROR: $*\n" >&2
+    exit 1
+}
+
+###############################################################################
+
+release_codename=`lsb_release -sc`
+
+# Update machine os/packages
+apt-get update &&
+apt-get upgrade
+
+# Install some common used packages
+apt-get --quiet --yes install vim man tree rsync \
+        htop lshw sysstat ntp \
+				screen \
+        iproute traceroute libevent-dev \
+        supervisor
+
+# Source control - SVN / Git
+apt-get --quiet --yes install subversion git-core
+
+# Setup Java
+if [ "$release_codename" = "lucid" ];
+then
+    # For Ubuntu 10.04, Need to enable partner repository
+    # for Sun Java to be available.
+    log "* Enabling partner repository for Sun JVM on Ubuntu 10.04"
+    apt-get --quiet --yes install python-software-properties &&
+    add-apt-repository "deb http://archive.canonical.com/ lucid partner" &&
+    apt-get update
+fi
+
+apt-get --quiet --yes install sun-java6-jdk &&
+update-java-alternatives -s java-6-sun
+
+# Configure sudoers, allow admin group
+perl -pi -e 's|#\s*%admin|%admin|' /etc/sudoers
+
+# Refresh filesys, cleanup etc
+updatedb
+
+log "All done."
